9108ea89f21b710aebaa30f43780f80225df88d3
Apurba <apurbaanikroy@gmail.com>
2026-05-20 14:16:15 +0600

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "884337910" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "912565559" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
